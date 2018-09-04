cc0707756db0c4b641d52f3c1b8c0e03f8f1cb8e
Imme2 <ericksilvarodriguez@gmail.com>
2018-09-04 03:36:53 -0600

    Added basic files and home/aboutus for the form

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+	// A little hack to get flex properties to stick.
+	// start with a hidden body, then hide everything then show the body.
+	$('.view').hide();
+	$("#first-screen").show();
+	$("body").show();
+
+	shown = "#first-screen"
+
+	// Events for buttons, not the most elegant way, but this is just an example.
+	$("#start-order").on('click',function(){
+
+		// Indicate we are no longer at home
+		$("#home-button").removeClass("active")
+
+
+		$("#first-screen").hide();
+		$("#order-screen").show();
+		shown = "#order-screen"
+	})
+
+	$("#home-button").on('click',function(){
+		// Switch the active class around
+		$(this).addClass("active");
+		$("#about-us").removeClass("active")
+
+		// Show and hide the views
+		$(shown).hide();
+		$("#first-screen").show();
+		shown = "#first-screen"
+	})
+	$("#about-us").on('click',function(){
+
+		// switch the active around		
+		$(this).addClass("active");
+		$("#home-button").removeClass("active")
+
+		// show and hide the views
+		$(shown).hide();
+		$("#about-us-screen").show();
+		shown = "#about-us-screen"
+
+	})
+	console.log("hello");
+})
